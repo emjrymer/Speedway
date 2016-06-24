ff4a343f2740a6946ad3f63622696f55d36d21d3
emjwivell <emjwivell@gmail.com>
2016-06-23 20:21:08 -0400

    addition of map images and footer styling

--- app/scripts/components/footer.jsx
+++ app/scripts/components/footer.jsx
@@ -7,17 +7,7 @@ var Footer = React.createClass({
   render: function(){
     return (
       <div className='footer'>
-          <div className="footer-filler col-md-6">
-              <p>Give us a Call! <a href="tel:864-271-1149"><strong> 864-271-1149</strong></a></p>
-          </div>
-          <div className="address col-md-6">
-            <a target="_blank" href="https://www.google.com/maps/place/Speedway+Packaging+and+Distribution/@34.8677333,-82.2420914,15z/data=!4m5!3m4!1s0x0:0xc666b001e6b56b9d!8m2!3d34.8677333!4d-82.2420914">
-             <address>
-                  1221B S. Batesville Rd.<br/>
-                  Greer, SC 29681
-            </address>
-            </a>
-          </div>
+          <div className="row">
             <div className="footer-background">
 
                     <div className="col-md-6" id="contact">
@@ -48,7 +38,9 @@ var Footer = React.createClass({
                         <button type="submit" className="btn btn-default submit-button">Submit</button>
                     </form>
                     </div>
-
+                    <div className="col-md-6">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13094.168077022532!2d-82.2420914!3d34.8677333!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc666b001e6b56b9d!2sSpeedway+Packaging+and+Distribution!5e0!3m2!1sen!2sus!4v1466726640614" width="100%" height="318" frameBorder="0" allowFullScreen></iframe>
+                </div>
                 <div className="copyright cr-second">
                     <center>
                         <span> Copyright &copy; 2016 Speedway Packaging and Distribution | <a href="http://emilywivell.com" target="_blank">emilywivell.com</a></span>
@@ -56,6 +48,7 @@ var Footer = React.createClass({
                 </div>
             </div>
         </div>
+    </div>
     );
   }
 });
